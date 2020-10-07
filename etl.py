1ee5fe6f3889266aea5154e73017df6a73b35de5
ABOUBACAR ALKA, Mahamadou Salissou <alkasalissou@gmail.com>
2020-10-07 23:46:57 +0200

    update insert queries

--- etl.py
+++ etl.py
@@ -5,23 +5,41 @@ from sql_queries import copy_table_queries, insert_table_queries
 
 def load_staging_tables(cur, conn):
     for query in copy_table_queries:
-        cur.execute(query)
-        conn.commit()
+        try:
+            cur.execute(query)
+            conn.commit()
+        except psycopg2.Error as e:
+            print(f"Can't execute query : {query}")
+            print(e)
 
 
 def insert_tables(cur, conn):
     for query in insert_table_queries:
-        cur.execute(query)
-        conn.commit()
+        try:
+            cur.execute(query)
+            conn.commit()
+        except psycopg2.Error as e:
+            print(f"Can't execute query : {query}")
+            print(e)
 
 
 def main():
     config = configparser.ConfigParser()
     config.read('dwh.cfg')
 
-    conn = psycopg2.connect("host={} dbname={} user={} password={} port={}".format(*config['CLUSTER'].values()))
-    cur = conn.cursor()
-    
+    try:
+        conn = psycopg2.connect("host={} dbname={} user={} password={} port={}".format(
+            config.get("CLUSTER", "HOST"),
+            config.get("CLUSTER", "DB_NAME"),
+            config.get("CLUSTER", "DB_USER"),
+            config.get("CLUSTER", "DB_PASSWORD"),
+            config.get("CLUSTER", "DB_PORT")
+        ))
+        cur = conn.cursor()
+    except psycopg2.Error as e:
+        print("Can't connect to database")
+        print(e)
+
     load_staging_tables(cur, conn)
     insert_tables(cur, conn)
 
--- sql_queries.py
+++ sql_queries.py
@@ -39,7 +39,7 @@ staging_events_table_create = ("""
 		userId INTEGER
 	)
 """)
-# ['num_songs', 'artist_id', 'artist_latitude', 'artist_longitude', 'artist_location', 'artist_name', 'song_id', 'title', 'duration', 'year']
+
 staging_songs_table_create = ("""
 	CREATE TABLE IF NOT EXISTS staging_songs (
 		num_songs INTEGER,
@@ -61,8 +61,8 @@ songplay_table_create = ("""
 		start_time TIMESTAMP REFERENCES time NOT NULL,
 		user_id VARCHAR REFERENCES users NOT NULL,
 		level VARCHAR,
-		song_id VARCHAR REFERENCES songs,
-		artist_id VARCHAR REFERENCES artists,
+		song_id VARCHAR REFERENCES songs NOT NULL,
+		artist_id VARCHAR REFERENCES artists NOT NULL,
 		session_id INTEGER,
 		location VARCHAR,
 		user_agent VARCHAR
@@ -130,6 +130,18 @@ staging_songs_copy = ("""
 # FINAL TABLES
 
 songplay_table_insert = ("""
+	INSERT INTO songplays (start_time, user_id, level, song_id, artist_id, session_id, location, user_agent)
+		SELECT ste.ts,
+			ste.userId, 
+			ste.level, 
+			sts.song_id, 
+			sts.artist_id, 
+			ste.sessionId,
+			ste.location,
+			ste.userAgent
+		FROM staging_events AS ste
+			LEFT JOIN staging_songs AS sts ON (ste.artist = sts.artist_name AND ste.song = sts.title)
+		WHERE ste.page = 'NextSong'
 """)
 
 user_table_insert = ("""
@@ -185,5 +197,4 @@ create_table_queries = [staging_events_table_create, staging_songs_table_create,
 drop_table_queries = [staging_events_table_drop, staging_songs_table_drop,
                       songplay_table_drop, user_table_drop, song_table_drop, artist_table_drop, time_table_drop]
 copy_table_queries = [staging_events_copy, staging_songs_copy]
-insert_table_queries = [songplay_table_insert, user_table_insert,
-                        song_table_insert, artist_table_insert, time_table_insert]
+insert_table_queries = [user_table_insert, time_table_insert, artist_table_insert, song_table_insert, songplay_table_insert]
